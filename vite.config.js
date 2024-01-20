b65f5318a9ce913f4abecda1ba37c287c114d0f3
Aparecio <aparecio.264317@novaliches.sti.edu.ph>
2024-01-20 11:11:19 +0800

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    base: '/'
-  },
-  resolve: {
-    alias: [
-      { find: /@\//, replacement: '/src/' },
-      { find: /\.gltf$/, replacement: '.gltf' },
-    ]
-  }
+  base: "/PCPulse/"
 })
